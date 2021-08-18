31102d0580f6ec00883ab9654a6ee225091eac17
Selfmade20 <ratshikombofarisani@gmail.com>
2021-08-18 12:17:51 +0200

    made changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState} from "react";
+import "./App.css";
+
+const formReducer = (state, event) => {
+  return {
+    ...state,
+    [event.name]: event.value,
+  };
+};
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [formData, setFormData] = useReducer(formReducer, {});
+  const [submitting, setSubmitting] = useState(false);
 
-export default App;
+  const handleSubmit = event => {
+    event.preventDefault();
+    setSubmitting(true);
+  
+    const handleChange = (event) => {
+      setFormData({
+        name: event.target.name,
+        value: event.target.value,
+      });
+    };
+  
+  
+    <div
+      className="wrapper"
+    >
+      <h1>YOU HAVE BEEN INVITED</h1>
+      {submitting && (
+        <div>
+          You are submitting the following:
+          <ul>
+            {Object.entries(formData).map(([name, value]) => (
+              <li key={name}>
+                <strong>{name}</strong>:{value.toString()}
+              </li>
+              
+            ))}
+          </ul>
+        </div>
+      )}
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>Name</p>
+          <input name="name" onChange={handleChange} />
+        </label>
+        <h2>Gender</h2>
+        <label className="container" />
+        Male
+        <input type="radio" checked="checked" name="radio" />
+        <span className="checkmark" />
+        <label className="container" />
+        Female
+        <input type="radio" name="radio" />
+        <span className="checkmark" />
+        <label className="container" />
+        Other
+        <input type="radio" name="radio" />
+        <span className="checkmark" />
+        <h2>Food</h2>
+        <label className="container" />
+        Sushi
+        <input
+          type="checkbox"
+          checked="checked"
+          name="checkbox"
+          onChange={handleChange}
+        />
+        <span className="checkmark" />
+        <label className="container" />
+        Pasta
+        <input type="checkbox" name="checkbox" onChange={handleChange} />
+        <span className="checkmark" />
+        <label className="container" />
+        Meaty Platter
+        <input type="checkbox" name="checkbox" onChange={handleChange} />
+        <span className="checkmark" />
+        <label>
+          <p>Age</p>
+        </label>
+        <input type="number" name="age"></input>
+ 
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  
+  }
+
+
+
+
+
+
+}
+  export default App;
